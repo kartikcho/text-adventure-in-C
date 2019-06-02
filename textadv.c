9424da51b1b17392ec5134e1b9b5e6a493b6f4bb
Kartik Choudhary <kartik.c918@gmail.com>
2019-06-02 12:21:03 +0530

    Refined code and removed search players function
    Reduced code lines by removing Search Players from menu and removed all associated code. Made program simpler and UI easy to use.

--- textadv.c
+++ textadv.c
@@ -6,7 +6,6 @@
 #include<ctype.h>                   //contains toupper(), tolower(),etc
 #include<dos.h>                     //contains _dos_getdate
 #include<time.h>
-//#include<bios.h>
 
 #define RETURNTIME 15
 
@@ -20,7 +19,7 @@ void closeapplication(void);
 int  getdata();
 int  checksr(int);
 int t(void);
-//void show_mouse(void);
+
 void issuerecord();
 void loaderanim();
 
@@ -47,14 +46,8 @@ struct players
 int id;
 char stname[20];
 char name[20];
-char Author[20];
-int quantity;
-float Price;
 int count;
-int rackno;
 char *cat;
-struct meroDate issued;
-struct meroDate duedate;
 };
 struct players a;
 int main()
@@ -64,28 +57,27 @@ getch();
 return 0;
 
 }
-void mainmenu()
+
+void mainmenu()         // Main Menu for the game
 {
-//loaderanim();
+
 system("cls");
-//    textbackground(13);
+
 int i;
 gotoxy(20,3);
 printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xDB A Mercenary in Tamriel \xDB\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-//    show_mouse();
+
 gotoxy(20,5);
-printf("\xDB\xDB\xDB\xDB\xB2 1. Start Game   "); //add book
+printf("\xDB\xDB\xDB\xDB\xB2 1. Start Game   "); 
 gotoxy(20,7);
-printf("\xDB\xDB\xDB\xDB\xB2 2. View Older Players"); //delete book
+printf("\xDB\xDB\xDB\xDB\xB2 2. View Older Players"); 
 gotoxy(20,9);
-printf("\xDB\xDB\xDB\xDB\xB2 3. Search Players"); //search book
+printf("\xDB\xDB\xDB\xDB\xB2 3. Exit"); 
 gotoxy(20,11);
-printf("\xDB\xDB\xDB\xDB\xB2 4. Exit"); //issue book
-gotoxy(20,13);
 printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-gotoxy(20,15);
+gotoxy(20,13);
 t();
-gotoxy(20,21);
+gotoxy(20,19);
 printf("Enter your choice:");
 switch(getch())
 {
@@ -96,9 +88,6 @@ case '2':
 viewplayers();
 break;
 case '3':
-searchplayers();
-break;
-case '4':
 {
 system("cls");
 gotoxy(16,3);
@@ -154,7 +143,7 @@ if(s==7)
 
 mainmenu() ;
 system("cls");
-fp=fopen("Bibek.dat","ab+");
+fp=fopen("playerdata.dat","ab+");
 if(getdata()==1)
 {
 a.cat=catagories[s-1];
@@ -173,122 +162,7 @@ printf("The character is sucessfully saved");
         }
 }
 
-void searchplayers()
-{
-system("cls");
-int d;
-printf("*****************************Search a Player*********************************");
-gotoxy(20,10);
-printf("\xDB\xDB\xDB\xB2 1. Search By SNo.");
-gotoxy(20,14);
-printf("\xDB\xDB\xDB\xB2 2. Search By Name");
-gotoxy( 15,20);
-printf("Enter Your Choice");
-fp=fopen("Bibek.dat","rb+"); //open file for reading propose
-rewind(fp);   //move pointer at the begining of file
-switch(getch())
-{
-case '1':
-{
-system("cls");
-gotoxy(25,4);
-printf("****Search Player By SNo.****");
-gotoxy(20,5);
-printf("Enter the Sno.:");
-scanf("%d",&d);
-gotoxy(20,7);
-printf("Searching........");
-while(fread(&a,sizeof(a),1,fp)==1)
-{
-if(a.id==d)
-{
-Sleep(2);
-gotoxy(20,7);
-printf("The Player is available");
-gotoxy(20,8);
-printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-gotoxy(20,9);
-printf("\xB2 ID:%d",a.id);gotoxy(47,9);printf("\xB2");
-gotoxy(20,10);
-printf("\xB2 Name:%s",a.name);gotoxy(47,10);printf("\xB2");
-gotoxy(20,11);
-printf("\xB2 Rack No:%d ",a.rackno);gotoxy(47,14);printf("\xB2");
-gotoxy(20,15);
-printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-findplayers='t';
-}
-
-}
-if(findplayers!='t')  //checks whether conditiion enters inside loop or not
-{
-gotoxy(20,8);
-printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-gotoxy(20,9);printf("\xB2");  gotoxy(38,9);printf("\xB2");
-gotoxy(20,10);
-printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-gotoxy(22,9);printf("\aNo Record Found");
-}
-gotoxy(20,17);
-printf("Try another search?(Y/N)");
-if(getch()=='y')
-searchplayers();
-else
-mainmenu();
-break;
-}
-case '2':
-{
-char s[15];
-system("cls");
-gotoxy(25,4);
-printf("****Search Player By Name****");
-gotoxy(20,5);
-printf("Enter Player Name:");
-scanf("%s",s);
-int d=0;
-while(fread(&a,sizeof(a),1,fp)==1)
-{
-if(strcmp(a.name,(s))==0) //checks whether a.name is equal to s or not
-{
-gotoxy(20,7);
-printf("Player Details");
-gotoxy(20,8);
-printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-gotoxy(20,9);
-printf("\xB2 SNo.:%d",a.id);gotoxy(47,9);printf("\xB2");
-gotoxy(20,10);
-printf("\xB2 Name:%s",a.name);gotoxy(47,10);printf("\xB2");
-gotoxy(20,11);
-printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-d++;
-}
-
-}
-if(d==0)
-{
-gotoxy(20,8);
-printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-gotoxy(20,9);printf("\xB2");  gotoxy(38,9);printf("\xB2");
-gotoxy(20,10);
-printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-gotoxy(22,9);printf("\aNo Record Found");
-}
-gotoxy(20,17);
-printf("Try another search?(Y/N)");
-if(getch()=='y')
-searchplayers();
-else
-mainmenu();
-break;
-}
-default :
-getch();
-searchplayers();
-}
-fclose(fp);
-}
-
-void viewplayers(void)  //show the list of book persists in library
+void viewplayers(void)  //show list of players
 {
 int i=0,j;
 system("cls");
@@ -297,7 +171,7 @@ printf("***************************Adventurers Before You***********************
 gotoxy(20,2);
 printf("Guild           SNo.           NAME");
 j=4;
-fp=fopen("Bibek.dat","rb");
+fp=fopen("playerdata.dat","rb");
 while(fread(&a,sizeof(a),1,fp)==1)
 {
 gotoxy(20,j);
